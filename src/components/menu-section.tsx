7172f30dd07b8e4f400083e3e8cda4bb90b2c298
Elton Pongilo <elton.pongilo@gmail.com>
2026-03-05 15:58:52 -0300

    feat: add new cake images and update menu items with descriptions and prices

--- src/components/menu-section.tsx
+++ src/components/menu-section.tsx
@@ -3,8 +3,9 @@ interface Menu {
   description: string;
   items: {
     name: string
+    description?: string;
     price: string
-    quantity?: number;
+    img?: string
   }[]
 }
 
@@ -24,7 +25,7 @@ export function MenuSection({ id, title, menu, description }: MenuSectionProps) 
         </h1>
         <p className="font-body text-base md:text-xl text-center text-white/80 mt-2 mb-5">{description}</p>
       </header>
-      <div className="max-w-2xl mx-auto space-y-10 mt-16" id={id}>
+      <div className="max-w-7xl mx-auto space-y-10 mt-16" id={id}>
         {menu.map((m) => (
           <div key={m.name} className="space-y-5">
             <div className="px-5 space-y-1">
@@ -33,18 +34,23 @@ export function MenuSection({ id, title, menu, description }: MenuSectionProps) 
                 <p className="font-body text-white/80 text-base">{m.description}</p>
               )}
             </div>
-            <div className="font-body text-white text-center text-lg">
+            <div className="font-body text-white text-base grid grid-cols-1 sm:px-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-5 gap-1">
               {m.items.map(item => (
-                  <div
-                    key={item.name}
-                    className="flex items-center gap-4 py-3 px-5 rounded-full duration-200 hover:bg-white/20 cursor-default"
-                  >
-                    <p className="flex-none">{item.name}</p>
-                    <hr className="flex-1 border-t border-white/50 border-dashed" />
-                    <p className="flex-none align-middle">
-                      <span className="text-sm">R$</span> {item.price}
-                    </p>
+                <div
+                  key={item.name}
+                  className="p-2 cursor-default flex sm:flex-col-reverse justify-between items-center duration-200 hover:bg-white/20 rounded-3xl"
+                >
+                  <div className="flex-1 p-3 space-y-0.5 w-full">
+                    <p className="font-bold">{item.name}</p>
+                    <p className="text-white/80">{item.description}</p>
+                    <p>{item.price}</p>
                   </div>
+                  <div className="w-30 sm:w-full aspect-square rounded-2xl overflow-hidden bg-white/10">
+                    {item.img && (
+                      <img src={item.img} alt={item.name} className="object-cover size-full" />
+                    )}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
