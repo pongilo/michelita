7bc35d745fc5d9917deca1e945e7dcd031380a0b
Elton Pongilo <elton.pongilo@gmail.com>
2026-03-27 20:04:50 -0300

    fix connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/../generated/prisma/client";
 
-const connectionString = process.env.DIRECT_URL ?? process.env.DATABASE_URL;
-
 export const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString }),
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
 });
